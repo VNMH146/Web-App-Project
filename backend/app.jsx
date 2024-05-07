79e2a1775f208ecb0595aff5a72d7fa7aeb5e2c8
Huy <vohuy060214@gmail.com>
2024-05-07 17:43:44 +0700

    create localhost:4000

--- backend/app.jsx
+++ backend/app.jsx
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+app.use("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+app.listen(4000, () => {
+  console.log(`Connected to localhost Port ${4000}`);
+});
+
+
+
