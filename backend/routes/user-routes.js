dae4124b13632ff231ab160031671147ed398f6f
Huy <vohuy060214@gmail.com>
2024-05-12 11:43:08 +0700

    update version nodemon, express, mongoose,and dotenv

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -3,6 +3,6 @@ import { getAllUsers } from "../controllers/user-controller.js";
 
 const userRouter = express.Router();
 
-userRouter.get("/alluser", getAllUsers);
+userRouter.get("/", getAllUsers);
 
 export default userRouter;
