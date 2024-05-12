ee1fb1665ec791398831bbe39a3bc5358aa364a7
Huy <vohuy060214@gmail.com>
2024-05-12 20:38:37 +0700

    add delete

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -60,3 +60,18 @@ export const updateUser = async (req, res, next) => {
   }
   res.status(200).json({ message: "User updated successfully" });
 }
+
+export const deleteUser = async (req, res, next) => {
+  const id = req.params.id;
+  let user;
+  try {
+    user = await User.findByIdAndRemove(id);
+  } catch (err) {
+    return console.log(err);
+  }
+
+  if (!user) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+  return res.status(200).json({ message: "User deleted successfully" });
+}
