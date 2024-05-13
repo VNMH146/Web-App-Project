9aa2ae2b45297936f66b04c030e54c85de11f43d
Huy <vohuy060214@gmail.com>
2024-05-13 17:54:28 +0700

    add signup, login for admin and token

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -11,11 +11,11 @@ export const addAdmin = async (req, res, next) => {
     return console.log(err);
   }
 
-  if (!existingAdmin) {
-    return res.status(500).json({ message: "Admin already exists" });
+  if (existingAdmin) {
+    return res.status(400).json({ message: "Admin already exists" });
   }
 
-
+  //define a new admin
   let admin;
   const hashedPassword = bcrypt.hashSync(password);
   try {
@@ -33,4 +33,30 @@ export const addAdmin = async (req, res, next) => {
   }
 
   return res.status(201).json({ admin });
-} 
+}
+
+export const adminLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || email.trim() === "" || !password || password.trim() === "") {
+    return res.status(422).json({ message: "Invalid inputs" });
+  }
+  let existingAdmin;
+  try {
+    existingAdmin = await Admin.findOne({ email });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!existingAdmin) {
+    return res.status(404).json({ message: "Admin not found" });
+
+  }
+  //compare the [assword]
+  const isPasswordCorrect = bcrypt.compareSync(password, existingAdmin.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res.status(200).json({ message: "Authentication completed successfully" });
+
+
+};
