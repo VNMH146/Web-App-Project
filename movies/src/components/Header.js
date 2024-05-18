f43e4483465d0ebbb7beb7966368658fae771166
MinhThang2002 <dongminhthang1205@gmail.com>
2024-05-18 23:34:27 +0700

    frontend update 18/5

--- movies/src/components/Header.js
+++ movies/src/components/Header.js
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {AppBar, Toolbar, Autocomplete, Tab} from '@mui/material'
 import MovieIcon from '@mui/icons-material/Movie';
 import {Box} from "@mui/system";
 import TextField from '@mui/material/TextField';
 import Tabs from '@mui/material/Tabs';
+import { getAllMovies } from "../api-helpers/api-helpers";
+import { Link } from "react-router-dom";
 
 
-const dummyArray = ["segg", "bo gia", "heo"];
+// const dummyArray = ["segg", "bo gia", "heo", "se gay"];
 const Header = () => {
-    return <AppBar>
-        <Toolbar>
-            <Box width = {"20%"}>
-                <MovieIcon />
-            </Box>
+    const [value, setValue] = useState(0);
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        getAllMovies()
+        .then((data) => setMovies(data.movies))
+        .catch(err => console.log(err))
+    }, []);
 
-            <Box width={'50%'} margin={'auto'}>
-                <Autocomplete
-                    freeSolo
-                    options={dummyArray.map((option) => option)}
-                    renderInput={(params) => <TextField variant="standard" {...params} label="Search Across Movie" />}
-                />
-            </Box>
+    return (
+        <AppBar sx = {{bgcolor:"#5b2d46"}}>
+            <Toolbar>
+                <Box width = {"20%"}>
+                    <MovieIcon />
+                </Box>
 
-            <Box display={'flex'}>
-                <Tabs>
-                    <Tab label = "Movie" />
-                    <Tab label = "Admin" />
-                    <Tab label = "Auth" />
+                <Box width={'30%'} margin={'auto'}>
+                    <Autocomplete
+                        freeSolo
+                        options={movies && movies.map((option) => option.title)}
+                        renderInput={(params) => <TextField 
+                            sx = {{input: {color:"white"}}} 
+                            variant="standard" 
+                            {...params} 
+                            placeholder="Search Across Movie" />}
+                    />
+                </Box>
 
-                </Tabs>
-            </Box>
-        </Toolbar>
-    </AppBar>;
+                <Box display={'flex'}>
+                    <Tabs textColor="inherit" indicatorColor="secondary" value={value} onChange={(e, val) => setValue(val)}>
+                        <Tab LinkComponent={Link} to="/movies" label = "Movies" />
+                        <Tab label = "Admin" LinkComponent={Link} to="/admin"/>
+                        <Tab label = "Auth" LinkComponent={Link} to="/auth"/>
+
+                    </Tabs>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
 };  
 
 export default Header;
